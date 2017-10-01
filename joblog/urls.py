abd42a091c8c28a9d58c79a34be53fce799405c0
Joseph Sungpil Choi <chousemath@gmail.com>
2017-10-01 16:12:27 +0900

    added linking

--- joblog/urls.py
+++ joblog/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from posts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name="home")
-]
+    url(r'^posts/(?P<post_id>[0-9]+)/$', views.show, name='show'),
+    url(r'^$', views.index, name="index"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . import models
 # Create your views here.
 
-def home(request):
+def index(request):
     posts = models.Post.objects.order_by('pub_date')
-    return render(request, 'posts/home.html', {'posts': posts})
+    return render(request, 'posts/index.html', {'posts': posts})
+
+def show(request, post_id):
+    post = get_object_or_404(models.Post, pk=post_id)
+    return render(request, 'posts/show.html', {'post': post})
