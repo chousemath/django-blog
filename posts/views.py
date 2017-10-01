df282fd2761684f8538cf3bcb063910e0b500c41
Joseph Sungpil Choi <chousemath@gmail.com>
2017-10-01 15:46:08 +0900

    added db model and model methods

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 
 def home(request):
-  return render(request, 'posts/home.html')
+    posts = models.Post.objects.order_by('pub_date')
+    return render(request, 'posts/home.html', {'posts': posts})
